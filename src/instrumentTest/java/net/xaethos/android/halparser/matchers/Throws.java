8798f089e3929009c54d5a72a504d9a869e44ccc
Diego García <diego@xaethos.net>
2013-08-08 11:33:42 +0800

    Throws matcher

---
+++ src/instrumentTest/java/net/xaethos/android/halparser/matchers/Throws.java
@@ -0,0 +1,38 @@
+package net.xaethos.android.halparser.matchers;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+public class Throws extends TypeSafeDiagnosingMatcher<Runnable> {
+    private final Class<? extends Throwable> mKlass;
+
+    public Throws(Class<? extends Throwable> klass) {
+        mKlass = klass;
+    }
+
+    @Override
+    protected boolean matchesSafely(Runnable runnable, Description mismatchDescription) {
+        try {
+            runnable.run();
+            mismatchDescription.appendText("did not throw.");
+        }
+        catch (Throwable t) {
+            if (mKlass.isInstance(t)) return true;
+            mismatchDescription.appendText("threw " + t.getClass().getSimpleName());
+        }
+        return false;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("throws " + mKlass.getSimpleName());
+    }
+
+    @Factory
+    public static <T> Matcher<Runnable> throwsA(Class<? extends Throwable> klass) {
+        return new Throws(klass);
+    }
+
+}
